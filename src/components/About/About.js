6d4cc5eaf0710b91b7df51688ab5d82cf9251688
Nathan Klundt <nathanryan001@gmail.com>
2017-11-08 16:49:50 -0700

    completed objectives

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom'
+import History from '../History/History'
+import Contact from '../Contact/Contact'
 
 export default class About extends Component {
 
     render() {
         return (
             <div>
-                <div className='subnav'>                    
+
+                <div className='subnav'>
+                    <Link to='/about' className='subnav_links'><h3>About</h3></Link>
+                    <Link to='/about/history' className='subnav_links'><h3>History</h3></Link>
+                    <Link to='/about/contact' className='subnav_links'><h3>Contact</h3></Link>
+
                 </div>
                 <div className='box'>
+                    <Switch>
+                        <Route path='/about/history' component={History}/>
+                        <Route path='/about/contact' component={Contact}/>
+                        <Route path='/about' render={()=>{
+                            return(
+                            <div>
+                                <h1 className='title'>About WestSide University:</h1>
+                                <p>Quare quoniam id quod est primum et quod huius imperi disciplinaeque maiorum proprium est
+                                    facere nondum audeo, faciam id quod est ad severitatem lenius et ad communem salutem
+                                    utilius. </p>
+                            </div>)
+                        }} />
+                    </Switch>
                 </div>
             </div>
         )
     }
-    
 }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props)
+    this.state = {
+      students: []
+    }
   }
-
+  componentDidMount() {
+    return axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res =>
+      (this.setState({ students: res.data })))
+  }
   render() {
+    const students = this.state.students.map((e, i) => (
+      <Link key={i} to={`/student/${e.id}`}><h3 >{e.first_name} {e.last_name}</h3></Link>
+    ))
     return (
-      <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
-
+      <div>
+        <div className="subnav">
+          <Link to='/'><button className='back-btn'>Return to class list</button></Link>
+        </div>
+        <div className="box">
+          <h1>{this.props.match.params.class}</h1>
+          <h2>ClassList:</h2>
+          {students}
+        </div>
       </div>
     )
   }
-    
+
 }
